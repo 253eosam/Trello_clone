b3c4a19664cb04bbd24d9367ac570333fe835bec
이성준 <donaldd@wemakeprice.com>
2020-08-03 18:56:10 +0900

    (style) Update board component UI

--- src/components/Board/Board.vue
+++ src/components/Board/Board.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="board">
-    <div class="board-header">
-      <el-tag class="board-tag" @click="onClickShowUpdateTagNameDialog" type="primary">{{tagName}}</el-tag>
+    <div>
+      <el-tag @click="onClickShowUpdateTagNameDialog" class="board-tag" type="primary">{{tagName}}</el-tag>
       <el-dialog title="Update Tag Name" :visible.sync="outerVisible">
         <div class="block">
           <el-input v-model="updateTagNameDialog.newTagName" placeholder="Please input your update Tag Name"></el-input>
@@ -12,16 +12,14 @@
         </div>
       </el-dialog>
     </div>
-    <div class="board-body">
-      <ul class="card-list">
-        <li v-for="(idx) in tasks" :key="idx">
-          <div>
-            <task :tid="idx"></task>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div class="board-footer" @click="onClickAddTask">
+    <ul class="card-list">
+      <li v-for="(idx) in tasks" :key="idx">
+        <div>
+          <task :tid="idx"></task>
+        </div>
+      </li>
+    </ul>
+    <div class="add-task-card" @click="onClickAddTask">
       <el-card shadow="hover">
         <span>+</span>
       </el-card>
@@ -76,25 +74,14 @@ export default {
 
 <style>
   .board {
+    overflow: auto;
+    height: 400px;
     padding: 10px;
     background: white;
     margin-left: 10px;
     margin-right: 10px;
-    margin-bottom: 20px;
+    margin-bottom: 100px;
     border-radius: 10px;
-  }
-
-  .board-header {
-    margin-bottom: 5px;
-  }
-
-  .board-body {
-    overflow: auto;
-    max-height: 400px;
-    margin-bottom: 20px;
-  }
-  .board-body>ul{
-    min-height: 10px;
   }
 
   .board-tag {
